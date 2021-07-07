d5fd085172b029a7998e37320b86b364a7d2d049
P4wner <p4wner@mineflare.net>
2021-07-07 06:17:03 +0200

    gg

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -62,9 +62,9 @@ for current_version in ${versions}; do
       sphinx-build -b html docs/source/ docs/build/html/${current_language}/${current_version} -D language="${current_language}"
  
       # PDF #
-      sphinx-build -b rinoh docs/source/ docs/build/rinoh -D language="${current_language}"
-      mkdir -p "${docroot}/${current_language}/${current_version}"
-      cp "docs/build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.pdf"
+      #sphinx-build -b rinoh docs/source/ docs/build/rinoh -D language="${current_language}"
+      #mkdir -p "${docroot}/${current_language}/${current_version}"
+      #cp "docs/build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.pdf"
  
       # EPUB #
       sphinx-build -b epub docs/source/ docs/build/epub -D language="${current_language}"
