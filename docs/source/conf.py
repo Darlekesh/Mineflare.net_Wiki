2c9e453d7949dbb836372f5d9c07125e41804a35
P4wner <p4wner@mineflare.net>
2021-07-07 07:16:17 +0200

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -375,4 +375,4 @@ html_context['github_version'] = 'master/docs/'
 # "_static" LINKS #
 ###################
 html_context['root_page'] = list()
-html_context['root_page'].append( ('site', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version) )
+html_context['root_page'].append( ('site', '/' +current_language+ '/' +current_version) )
